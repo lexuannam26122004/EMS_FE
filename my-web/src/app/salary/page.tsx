23af3053285cd5d0487166c05cc14b691d86d450
MinhNoProUIT <154415039+MinhNoProUIT@users.noreply.github.com>
2024-10-24 21:19:32 +0700

    fix

--- my-web/src/app/salary/page.tsx
+++ my-web/src/app/salary/page.tsx
@@ -1,9 +1,7 @@
-import Layout from "@/components/Layout";
+// import Layout from "@/components/Layout";
 
 export default function Salary(){
-    return <Layout>
-            <h1>
-        salary
-    </h1>
-        </Layout>
+    return <h1>
+            salary
+        </h1>
 }
