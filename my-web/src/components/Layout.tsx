7bcaa683aa26446424acc7bf6bf35b17a1b3fa54
vanphong19 <phongdv1901@gmail.com>
2024-10-31 21:56:12 +0700

    holiday

--- my-web/src/components/Layout.tsx
+++ my-web/src/components/Layout.tsx
@@ -4,10 +4,11 @@ import Sidebar, { SidebarItem } from '@/components/Sidebar'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import 'react-perfect-scrollbar/dist/css/styles.css'
 // import { IconButton } from '@mui/material'
-import { Wallet} from 'lucide-react'
+import { Wallet } from 'lucide-react'
 import { Unlock, Calendar, Settings } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import i18n from '@/i18n/i18n'
+import { BiCalendarStar } from 'react-icons/bi'
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const pathname = usePathname() // Lấy đường dẫn hiện tại
@@ -20,12 +21,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div style={{ display: 'flex', height: '100vh' }}>
             <Sidebar>
-                <SidebarItem
-                    icon={<Calendar />}
-                    text='Employee'
-                    route='/employee'
-                    active={pathname === '/employee'}
-                />
+                <SidebarItem icon={<Calendar />} text='Employee' route='/employee' active={pathname === '/employee'} />
                 <SidebarItem
                     icon={<Calendar />}
                     text='Timekeeping'
@@ -41,6 +37,17 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 />
                 <SidebarItem icon={<Settings />} text='Settings' route='/' active={pathname === '/'} />
                 <SidebarItem icon={<Wallet />} text='Salary' route='/salary' active={pathname === '/salary'} />
+                <SidebarItem
+                    icon={
+                        <BiCalendarStar
+                            size={24}
+                            style={{ display: 'flex', alignItems: 'center' }} // Căn giữa và khoảng cách giữa icon và text
+                        />
+                    }
+                    text='Holiday'
+                    route='/holiday'
+                    active={pathname === '/holiday'}
+                />
             </Sidebar>
             <PerfectScrollbar style={{ flex: 1 }}>
                 <main style={{ padding: '0 5px', height: '100%', overflowY: 'scroll', backgroundColor: '#eeeeff' }}>
