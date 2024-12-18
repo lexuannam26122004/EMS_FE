3ce50658c9a24913d4d3bd35857b69b752a935cb
ASUS PC <22520927@gm.uit.edu.vn>
2024-12-18 11:53:59 +0700

    code

--- my-web/next.config.mjs
+++ my-web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+        domains: ['assets.minimals.cc'], 
+    },
 }
 
 export default nextConfig
--- my-web/src/app/admin/employee/create-employee/page.tsx
+++ my-web/src/app/admin/employee/create-employee/page.tsx
@@ -13,6 +13,7 @@ import {
     Checkbox,
     ListItemText
 } from '@mui/material'
+import Image from 'next/image';
 import { useTranslation } from 'react-i18next'
 import { SaveIcon, XIcon, RefreshCcwIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation'
@@ -53,6 +54,12 @@ const CreateEmployeePage = () => {
     const [roles, setRoles] = useState<string[]>([])
     const [isExistingUser, setIsExistingUser] = useState(false)
     const [isExistingEmail, setIsExistingEmail] = useState(false)
+
+    const [previewSrc, setPreviewSrc] = useState<string>(
+        'https://assets.minimals.cc/public/assets/images/mock/avatar/avatar-2.webp'
+    )
+
+    const [file, setFile] = useState<File | null>(null)
 
     const toast = useToast()
     const [isSubmit, setIsSubmit] = useState(false)
@@ -190,8 +197,138 @@ const CreateEmployeePage = () => {
 
     if (isUsersLoading || isRoleLoading || isDepartmentLoading) return <div>Loading...</div>
 
+    const triggerFileInput = () => {
+        document.getElementById('fileInput')?.click()
+    }
+
+    // Handle image preview
+    const previewImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files ? event.target.files[0] : null
+
+        if (file) {
+            // Check file size
+            if (file.size > 3 * 1024 * 1024) {
+                alert('File size must not exceed 3MB!')
+                return
+            }
+
+            const reader = new FileReader()
+            reader.onload = (e: ProgressEvent<FileReader>) => {
+                setPreviewSrc(e.target?.result as string)
+                setFile(file)
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+
     return (
         <Box sx={{ width: '100%' }}>
+            <Box
+                sx={{
+                    fontFamily: 'Arial, sans-serif',
+                    backgroundColor: '#f9f9f9',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    margin: 0
+                }}
+            >
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        background: '#fff',
+                        padding: '15px',
+                        borderRadius: '10px',
+                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                        maxWidth: '250px'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            cursor: 'pointer'
+                        }}
+                    >
+                        <input
+                            type='file'
+                            id='fileInput'
+                            accept='image/*'
+                            onChange={previewImage}
+                            style={{ display: 'none' }}
+                        />
+
+                        <Box
+                            sx={{
+                                width: '120px',
+                                height: '120px',
+                                borderRadius: '50%',
+                                overflow: 'hidden',
+                                margin: '0 auto 8px',
+                                backgroundColor: '#f0f0f0',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                position: 'relative',
+                                transition: 'box-shadow 0.3s ease'
+                            }}
+                            onMouseEnter={() => (document.getElementById('updatePhoto')!.style.display = 'flex')}
+                            onMouseLeave={() => (document.getElementById('updatePhoto')!.style.display = 'none')}
+                        >
+                            <Image
+                                src={previewSrc}
+                                alt='Avatar'
+                                layout='fill' // This makes the image cover the parent container
+                                objectFit='cover' // Ensure the image maintains the same aspect ratio
+                            />
+
+                            <Box
+                                id='updatePhoto'
+                                sx={{
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    width: '100%',
+                                    height: '100%',
+                                    background: 'rgba(0, 0, 0, 0.3)',
+                                    display: 'none',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    fontSize: '14px',
+                                    cursor: 'pointer',
+                                    flexDirection: 'column',
+                                    textAlign: 'center'
+                                }}
+                                onClick={triggerFileInput}
+                            >
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='36'
+                                    height='36'
+                                    viewBox='0 0 24 24'
+                                    fill='white'
+                                >
+                                    <path d='M12 10.25a.75.75 0 0 1 .75.75v1.25H14a.75.75 0 0 1 0 1.5h-1.25V15a.75.75 0 0 1-1.5 0v-1.25H10a.75.75 0 0 1 0-1.5h1.25V11a.75.75 0 0 1 .75-.75'></path>
+                                    <path d='M9.778 21h4.444c3.121 0 4.682 0 5.803-.735a4.4 4.4 0 0 0 1.226-1.204c.749-1.1.749-2.633.749-5.697s0-4.597-.749-5.697a4.4 4.4 0 0 0-1.226-1.204c-.72-.473-1.622-.642-3.003-.702c-.659 0-1.226-.49-1.355-1.125A2.064 2.064 0 0 0 13.634 3h-3.268c-.988 0-1.839.685-2.033 1.636c-.129.635-.696 1.125-1.355 1.125c-1.38.06-2.282.23-3.003.702A4.4 4.4 0 0 0 2.75 7.667C2 8.767 2 10.299 2 13.364s0 4.596.749 5.697c.324.476.74.885 1.226 1.204C5.096 21 6.657 21 9.778 21M16 13a4 4 0 1 1-8 0a4 4 0 0 1 8 0m2-3.75a.75.75 0 0 0 0 1.5h1a.75.75 0 0 0 0-1.5z'></path>
+                                </svg>
+                                <span style={{ color: 'white' }}>Update Avatar</span>
+                            </Box>
+                        </Box>
+                    </Box>
+                    {/* Instructions */}
+                    <Box
+                        sx={{
+                            fontSize: '12px',
+                            color: '#666',
+                            marginTop: '8px'
+                        }}
+                    >
+                        Allowed: *.jpeg, *.jpg, *.png, *.gif
+                        <br />
+                        <small style={{ fontSize: '10px', color: '#999' }}>Max size: 3MB</small>
+                    </Box>
+                </Box>
+            </Box>
+
             <Paper
                 sx={{
                     width: '100%',
