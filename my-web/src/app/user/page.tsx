c110eff380655a90dbf8dc9568d9e56ea13f5d6d
ASUS PC <22520927@gm.uit.edu.vn>
2024-12-19 16:39:43 +0700

    fix-UIuser

--- my-web/src/app/user/page.tsx
+++ my-web/src/app/user/page.tsx
@@ -1,36 +1,42 @@
 'use client'
 
-import { Box, Paper, TableContainer, Table, TableRow, TableBody, TableCell, Avatar } from '@mui/material'
+import { Box, Paper, TableContainer, Table, TableRow, TableBody, TableCell, Avatar, Tabs, Tab } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-
+import React, { useState, useEffect } from 'react'
 const DetailModal = () => {
     const { t } = useTranslation('common')
 
-    const backgroundImageUrl = `/background/${1}.jpg`
+    
+    const [backgroundImageUrl, setBackgroundImageUrl] = useState('');
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * 10) + 1;
+        const newBackgroundImageUrl = `/background/${randomIndex}.jpg`;
+        setBackgroundImageUrl(newBackgroundImageUrl);
+    }, []);
+
+    const [selectedTab, setSelectedTab] = useState(0)
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setSelectedTab(newValue)
+        console.log('Selected Tab:', newValue)
+    }
 
     return (
         <Paper
             elevation={0}
             sx={{
                 width: '100%',
-                height: '100vh',
-                position: 'absolute',
-                overflowY: 'auto',
-                top: '50%',
-                left: '50%',
-                backgroundColor: 'var(--background-color)',
                 border: '1px solid var(--border-color)',
                 borderRadius: '10px',
-                transform: 'translate(-50%, -50%)',
-                '&::-webkit-scrollbar': {
-                    display: 'none'
-                }
+                backgroundColor: 'var(--border-color)'
             }}
         >
             <Box
                 sx={{
+                    borderRadius: '15px',
+                    backgroundColor: 'var(--background-item)',
                     width: '100%',
-                    maxHeight: '80vh',
                     overflow: 'auto',
                     '&::-webkit-scrollbar-corner': {
                         borderRadius: '10px'
@@ -112,74 +118,92 @@ const DetailModal = () => {
                 <Box
                     sx={{
                         display: 'flex',
-                        justifyContent: 'flex-start',
-                        padding: '5px',
-
-                        borderTop: '1px solid #ddd',
-                        borderBottom: '1px solid #ddd'
+                        justifyContent: 'flex-end',
+                        padding: '20px 30px',
+                        marginTop: '-50px'
                     }}
-                ></Box>
+                >
+                    <Tabs
+                        value={selectedTab}
+                        onChange={handleTabChange}
+                        aria-label='right-aligned tabs'
+                        sx={{
+                            borderBottom: 1,
+                            borderColor: 'divider'
+                        }}
+                    >
+                        <Tab label='Cá Nhân' />
+                        <Tab label='Hợp Đồng' />
+                        <Tab label='Phúc Lợi' />
+                        <Tab label='Kỉ Luật' />
+                    </Tabs>
+                </Box>
             </Box>
 
-            <TableContainer
+            <Box
                 sx={{
-                    width: '100%',
+                    margin: '20px',
+                    width: '50%',
                     overflow: 'hidden',
                     borderRadius: '15px',
-                    backgroundColor: 'var(--background-item)'
+                    backgroundColor: 'var(--background-item)',
+                    marginLeft: 'auto',
+                    padding: '20px'
                 }}
             >
-                <Table>
-                    <TableBody>
-                        {[
-                            { label: t('Họ tên đầy đủ'), value: 'aspnetuser.FullName' },
-                            { label: t('Tên phòng ban'), value: 'aspnetuser.DepartmentName ' },
-                            { label: t('Tên tài khoản'), value: 'aspnetuser.UserName' },
-                            {
-                                label: t('Chức vụ trong công ty'),
-                                value: 'aspnetuser.Roles'
-                            },
-                            {
-                                label: t('Giới tính'),
-                                value: 'aspnetuser.Gender '
-                            },
-                            { label: t('Địa chỉ thường trú'), value: 'aspnetuser.Address ' },
-                            {
-                                label: t('Ngày sinh'),
-                                value: 'aspnetuser.Birthday'
-                            },
-                            { label: t('Email'), value: 'aspnetuser.Email' },
-                            { label: t('Số điện thoại'), value: 'aspnetuser.PhoneNumber' },
-                            { label: t('Ghi chú'), value: 'aspnetuser.Note ' }
-                        ].map((item, index) => (
-                            <TableRow key={index}>
-                                <TableCell
-                                    sx={{
-                                        fontSize: '16px',
-                                        fontWeight: 'bold',
-                                        whiteSpace: 'nowrap',
-                                        color: 'var(--text-color)',
-                                        borderBottom: 'none',
-                                        paddingLeft: '100px',
-                                        width: '40%'
-                                    }}
-                                >
-                                    {item.label}:
-                                </TableCell>
-                                <TableCell
-                                    sx={{
-                                        color: 'var(--text-color)',
-                                        fontSize: '16px',
-                                        borderBottom: 'none'
-                                    }}
-                                >
-                                    {item.value}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            {[
+                                { label: t('Họ tên đầy đủ'), value: 'aspnetuser.FullName' },
+                                { label: t('Tên phòng ban'), value: 'aspnetuser.DepartmentName ' },
+                                { label: t('Tên tài khoản'), value: 'aspnetuser.UserName' },
+                                {
+                                    label: t('Chức vụ trong công ty'),
+                                    value: 'aspnetuser.Roles'
+                                },
+                                {
+                                    label: t('Giới tính'),
+                                    value: 'aspnetuser.Gender '
+                                },
+                                { label: t('Địa chỉ thường trú'), value: 'aspnetuser.Address ' },
+                                {
+                                    label: t('Ngày sinh'),
+                                    value: 'aspnetuser.Birthday'
+                                },
+                                { label: t('Email'), value: 'aspnetuser.Email' },
+                                { label: t('Số điện thoại'), value: 'aspnetuser.PhoneNumber' },
+                                { label: t('Ghi chú'), value: 'aspnetuser.Note ' }
+                            ].map((item, index) => (
+                                <TableRow key={index}>
+                                    <TableCell
+                                        sx={{
+                                            fontSize: '16px',
+                                            fontWeight: 'bold',
+                                            whiteSpace: 'nowrap',
+                                            color: 'var(--text-color)',
+                                            borderBottom: 'none',
+                                            paddingLeft: '100px',
+                                            width: '40%'
+                                        }}
+                                    >
+                                        {item.label}:
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            color: 'var(--text-color)',
+                                            fontSize: '16px',
+                                            borderBottom: 'none'
+                                        }}
+                                    >
+                                        {item.value}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </Paper>
     )
 }
