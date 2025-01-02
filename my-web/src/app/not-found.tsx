7ba11582f111b1a4f5d60e5e527e3a23994248d9
lexuannam26122004 <161745873+lexuannam26122004@users.noreply.github.com>
2025-01-02 16:15:01 +0700

    reset password

--- my-web/src/app/not-found.tsx
+++ my-web/src/app/not-found.tsx
@@ -27,42 +27,9 @@ const Custom404Page: FC = () => {
         >
             <Box
                 sx={{
-                    position: 'absolute',
-                    top: 0,
-                    height: '70px',
-                    left: 24,
-                    right: 24,
-                    backdropFilter: 'blur(10px)',
-                    backgroundColor: 'var(--header-maim-color)',
-                    zIndex: 1100
-                }}
-            >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        width: '100%',
-                        height: '100%'
-                    }}
-                >
-                    <img
-                        onClick={handleClick}
-                        src='/images/logo.png'
-                        style={{
-                            cursor: 'pointer',
-                            height: '50px',
-                            transition: 'all 300ms ease-in-out'
-                        }}
-                    />
-                </Box>
-            </Box>
-
-            <Box
-                sx={{
                     flex: 1,
-                    padding: '100px 0 80px',
-                    margin: '0 400px',
+                    padding: '40px 0 40px',
+                    margin: '0 300px',
                     textAlign: 'center',
                     color: '#fff'
                 }}
@@ -70,7 +37,7 @@ const Custom404Page: FC = () => {
                 <Typography variant='h3' sx={{ fontWeight: 'bold', color: 'var(--text-color)' }}>
                     Sorry, page not found!
                 </Typography>
-                <Typography variant='h6' sx={{ mt: 4, color: 'var(--sub-title-color)' }}>
+                <Typography variant='h6' sx={{ mt: 4, fontSize: '17px', color: 'var(--sub-title-color)' }}>
                     Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to
                     check your spelling.
                 </Typography>
